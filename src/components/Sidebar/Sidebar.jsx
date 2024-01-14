03cda2b596fecd2c5f6f507464cd70f969baeea9
Minnie Su <heyminniesu@gmail.com>
2024-01-14 12:07:09 -0500

    Design Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,13 @@
-import { Avatar, Box, Flex, Tooltip } from "@chakra-ui/react";
-import { Link, Link as RouterLink } from "react-router-dom";
+import { Avatar, Box, Flex, Tooltip, chakra } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 import { CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, SearchLogo } from "../../assets/constants";
 import { AiFillHome } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 // sidebar has one flex holding the icons and one flex holding the texts.
+
+// using Chakra UI's chakra utility function. This allows you to apply Chakra UI styling props to the RouterLink
+const ChakraLink = chakra(RouterLink);
+
 const Sidebar = () => {
   const sideBarItems = [
     { icon: <AiFillHome size={25} />, text: "Home", link: "/" },
@@ -24,21 +29,31 @@ const Sidebar = () => {
     >
       <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
         {/* we can add both a style to this link as a normal chakra component && a functionality to it from react-router-dom */}
-        <Box display={{ base: "none", md: "block" }} pb={10}>
-          <Link to={"/"} as={RouterLink} pl={2} cursor={"pointer"}>
+        <Box display={{ base: "none", md: "block" }}>
+          <ChakraLink to={"/"} as={RouterLink} pl={2} cursor={"pointer"}>
             <InstagramLogo />
-          </Link>
+          </ChakraLink>
         </Box>
-        <Box display={{ base: "block", md: "none" }}>
-          <Link to={"/"} as={RouterLink} pl={2} borderRadius={6} _hover={{ bg: "whiteAlpha.200" }} w={10} cursor={"pointer"}>
+        <Box pt={5}>
+          <ChakraLink
+            to={"/"}
+            as={RouterLink}
+            borderRadius={6}
+            _hover={{ bg: "whiteAlpha.200" }}
+            w={10}
+            cursor={"pointer"}
+            display={{ base: "block", md: "none" }}
+            p={2}
+            mb={50}
+          >
             <InstagramMobileLogo />
-          </Link>
+          </ChakraLink>
 
           <Flex direction={"column"} gap={5} cursor={"pointer"}>
             {sideBarItems.map((item, index) => {
               return (
                 <Tooltip key={index} hasArrow label={item.text} placement={"right"} ml={1} openDelay={500} display={{ base: "block", md: "none" }}>
-                  <Link
+                  <ChakraLink
                     display={"flex"}
                     to={item.link || null}
                     as={RouterLink}
@@ -48,16 +63,36 @@ const Sidebar = () => {
                     borderRadius={6}
                     p={2}
                     w={{ base: 10, md: "full" }}
+                    justifyContent={{ base: "center", md: "flex-start" }}
                   >
                     {item.icon}
 
                     <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
-                  </Link>
+                  </ChakraLink>
                 </Tooltip>
               );
             })}
           </Flex>
         </Box>
+        <Tooltip hasArrow label={"Logout"} placement={"right"} ml={1} openDelay={500} display={{ base: "block", md: "none" }}>
+          <ChakraLink
+            display={"flex"}
+            to={"/auth"}
+            as={RouterLink}
+            alignItems={"center"}
+            gap={4}
+            _hover={{ bg: "whiteAlpha.400" }}
+            borderRadius={6}
+            p={2}
+            w={{ base: 10, md: "full" }}
+            justifyContent={{ base: "center", md: "flex-start" }}
+            mt={"auto"}
+          >
+            <BiLogOut size={25}></BiLogOut>
+
+            <Box display={{ base: "none", md: "block" }}>Logout</Box>
+          </ChakraLink>
+        </Tooltip>
       </Flex>
     </Box>
   );
