f6abc104068851aa760e1b1fbe15ff4fb6136f2e
Minnie Su <heyminniesu@gmail.com>
2024-01-18 12:14:36 -0500

    Update AuthForm

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,58 +1,20 @@
-import { Box, Button, Flex, Image, Input, Text, VStack } from "@chakra-ui/react";
+import { Box, Flex, Image, Text, VStack } from "@chakra-ui/react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import GoogleAuth from "./GoogleAuth";
+import Login from "./Login";
+import Signup from "./Signup";
 
 const AuthForm = () => {
   // create useState to determine sign up or log in. displays additional inputs "confirm email" and sign up botton if signing up, display only email and password and login button if logging in
   const [isLogin, setIsLogin] = useState(true);
-  //   create useState to copy the email and password. create an object inputs to store the data and pass it to setInputs.
-  const [inputs, setInputs] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-  //   useNavigate hook returns a function that lets you navigate programmatically
-  const navigate = useNavigate();
-  const handleAuth = () => {
-    if (!inputs.email || !inputs.password) {
-      alert("Please fill all the field!");
-      return;
-    }
-    navigate("/");
-  };
+
   return (
     <>
       <Box border={"1px solid gray"} borderRadius={4} padding={5}>
         {/* use vstack to stack the instgram logo, inputs for email and password vertically */}
         <VStack spacing={4}>
           <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram" />
-          <Input
-            placeholder="Email"
-            fontSize={14}
-            type="email"
-            value={inputs.email}
-            onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-          />
-          <Input
-            placeholder="Password"
-            fontSize={14}
-            type="password"
-            value={inputs.password}
-            onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-          />
-          {isLogin ? null : (
-            <Input
-              placeholder="Confirm Password"
-              fontSize={14}
-              type="password"
-              value={inputs.confirmPassword}
-              onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
-            />
-          )}
-
-          <Button w={"full"} colorScheme="blue" size={"sm"} fontStyle={14} onClick={handleAuth}>
-            {isLogin ? "Log in" : "Sign up"}
-          </Button>
+          {isLogin ? <Login /> : <Signup />}
 
           {/* ----------------OR---------------- */}
           {/* alignItems attribute helps to align index with the text, setting margin y, gap and width  */}
@@ -64,13 +26,7 @@ const AuthForm = () => {
             </Text>
             <Box flex={2} h={"1px"} bg={"gray.400"} />
           </Flex>
-
-          <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
-            <Image src="/google.png" w={5} alt="google logo" />
-            <Text mx={2} color={"blue.500"}>
-              Log in with Google
-            </Text>
-          </Flex>
+          <GoogleAuth />
         </VStack>
       </Box>
 
---
+++ src/components/AuthForm/Login.jsx
@@ -0,0 +1,36 @@
+import { Button, Input } from "@chakra-ui/react";
+import { useState } from "react";
+
+const Login = () => {
+  //   create useState to copy the email and password. create an object inputs to store the data and pass it to setInputs.
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <>
+      <Input
+        placeholder="Email"
+        fontSize={14}
+        size={"sm"}
+        type="email"
+        value={inputs.email}
+        onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+      />
+      <Input
+        placeholder="Password"
+        fontSize={14}
+        size={"sm"}
+        type="password"
+        value={inputs.password}
+        onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+      />
+      <Button w={"full"} colorScheme="blue" size={"sm"} fontStyle={14}>
+        Log in
+      </Button>
+    </>
+  );
+};
+
+export default Login;
