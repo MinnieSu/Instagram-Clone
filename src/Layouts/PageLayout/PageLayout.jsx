eb7052d6d8510e622cdba470fa52ebb4d7f4020a
Minnie Su <heyminniesu@gmail.com>
2024-01-21 11:44:00 -0500

    useAuthState Example

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,16 +1,23 @@
 // instead of adding the sidebar component to every page, we can add it only once to the PageLayout component
 // and wrap the children with it. This way, we can have a sidebar on every pageexcept the AuthPage.
+// useAuthState to retrieve and monitor the authentication state from Firebase. (react-firebase-hooks)
 
 import { Box, Flex } from "@chakra-ui/react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useLocation } from "react-router-dom";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import { auth } from "../../firebase/firebase";
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
+  // check in the backsground if a user is logged in or not
+  const [user, loading, error] = useAuthState(auth);
+  // render sidebar only when user is logged in. no sidebar when on authpage.
+  const canRenderSidebar = pathname !== "/auth" && user;
   return (
     <Flex>
       {/* sidebar on the left */}
-      {pathname !== "/auth" ? (
+      {canRenderSidebar ? (
         <Box w={{ base: "70px", md: "240px" }}>
           <Sidebar />
         </Box>
