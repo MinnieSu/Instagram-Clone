6d0a31073b347c21a0934b0bb48f2dd596961950
Minnie Su <heyminniesu@gmail.com>
2024-02-26 17:01:32 -0500

    Search For User Profiles

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,35 +1,51 @@
 import { Avatar, Box, Button, Flex, VStack } from "@chakra-ui/react";
-import { useState } from "react";
+import UseFollowUser from "../../hooks/UseFollowUser";
+import useAuthStore from "../../store/authStore";
 
-const SuggestedUser = ({ name, followers, avatar }) => {
-  const [isFollowed, setIsFollowed] = useState(false);
+const SuggestedUser = ({ user, setUser }) => {
+  const { isFollowing, isUpdating, handleFollowUser } = UseFollowUser(user.uid);
+  const authUser = useAuthStore((state) => state.user);
 
+  const onFollowUser = async () => {
+    await handleFollowUser();
+    // update the user's followers if the authUser clicks the follow or unfollow button.
+    setUser({
+      ...user,
+      followers: isFollowing
+        ? user.followers.filter((follower) => follower.uid !== authUser.uid)
+        : [...user.followers, authUser],
+    });
+  };
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
       <Flex alignItems={"center"} gap={2}>
-        <Avatar src={avatar} name={name} size={"md"} />
+        <Avatar src={user.profilePicURL} size={"md"} />
         <VStack spacing={2} alignItems={"flex-start"}>
           <Box fontSize={12} fontWeight={"bold"}>
-            {name}
+            {user.fullName}
           </Box>
           <Box fontSize={11} color={"gray.500"}>
-            {followers} followers
+            {user.followers.length} followers
           </Box>
         </VStack>
       </Flex>
-      <Button
-        fontSize={13}
-        bg={"transparent"}
-        p={0}
-        h={"max-content"}
-        fontWeight={"medium"}
-        color={"blue.400"}
-        cursor={"pointer"}
-        _hover={{ color: "white" }}
-        onClick={() => setIsFollowed(!isFollowed)}
-      >
-        {isFollowed ? "Unfollow" : "Follow"}
-      </Button>
+      {/* display follow/unfollow button for other users, because we cannot follow ourselves. */}
+      {authUser.uid !== user.uid && (
+        <Button
+          fontSize={13}
+          bg={"transparent"}
+          p={0}
+          h={"max-content"}
+          fontWeight={"medium"}
+          color={"blue.400"}
+          cursor={"pointer"}
+          _hover={{ color: "white" }}
+          onClick={onFollowUser}
+          isLoading={isUpdating}
+        >
+          {isFollowing ? "Unfollow" : "Follow"}
+        </Button>
+      )}
     </Flex>
   );
 };
---
+++ src/hooks/useSearchUser.js
@@ -0,0 +1,33 @@
+import { collection, getDocs, query, where } from "@firebase/firestore";
+import { useState } from "react";
+import { firestore } from "../firebase/firebase";
+import useShowToast from "./useShowToast";
+
+const useSearchUser = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const showToast = useShowToast();
+  //   search user by username
+  const getUserProfile = async (username) => {
+    setIsLoading(true);
+    try {
+      //   get all the users with the given username
+      const q = query(collection(firestore, "users"), where("username", "==", username));
+      const querySnapshot = await getDocs(q);
+      //  if no user is found
+      if (querySnapshot.empty) return showToast("Error", "User Not Found", "error");
+      // if a user is found, update the user's followers if the authUser clicks the follow or unfollow button.
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      showToast("Error", error.message, "error");
+      setUser(null);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return { isLoading, user, setUser, getUserProfile };
+};
+
+export default useSearchUser;
