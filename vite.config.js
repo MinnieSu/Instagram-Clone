37a60105678ad3bfb16a1a7ce5143d3663ea606b
Minnie Su <heyminniesu@gmail.com>
2024-03-31 10:28:06 -0400

    Restore zustand internal dependency

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["zustand"], // Specify zustand as an external dependency
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["zustand"], // Specify zustand as an external dependency
+  //   },
+  // },
 });
